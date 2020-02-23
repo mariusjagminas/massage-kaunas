68a7a0b88f76ae5f790b8ae4519f8e26edb9f8f8
mariusjagminas <mariusjagminas.it@gmail.com>
2020-02-23 14:39:24 +0100

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout">
+    <header class="header"></header>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>Main Page</Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Masažo salonas Kaune"
+  }
+};
+</script>
+
+<style>
+</style>
