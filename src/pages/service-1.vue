d1b7b855b3fbb177a582c6597209c6797a1f3fe7
mariusjagminas <mariusjagminas.it@gmail.com>
2020-03-11 09:22:36 +0100

    Add provisional pricing & service-1 pages

---
+++ src/pages/service-1.vue
@@ -0,0 +1,69 @@
+<template>
+  <Layout>
+    <div class="container">
+      <Decoration :large="true" />
+      <div class="wrapper">
+        <h1>
+          Cia galetu buti platesnis masazo aprasymas. Pvz:
+          <br />
+          <br />a) Kiek laiko trunka
+          <br />b) Koks poveikis sveikatai
+          <br />c) Kaina
+          <br />d) Kiek sesiju reikia norint pasiekti reikiama rezultata
+          <br />e) ir t.t.
+          <br />
+          <br />(sio puslapio gali ir nebut)
+        </h1>
+        <a href="/">Grizti i pagrindini</a>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Decoration from "@/components/Decoration/Decoration.vue";
+
+export default {
+  metaInfo: {
+    title: "404"
+  },
+  components: {
+    Decoration
+  }
+};
+</script >
+
+<style lang="scss" scoped>
+.container {
+  background-color: $color-background;
+}
+
+.wrapper {
+  @include container;
+  @include flex(center, center, column);
+  min-height: rem(500);
+  padding-bottom: rem(30);
+}
+
+p,
+h1,
+a {
+  @include font($font-family-header, bold);
+}
+
+p,
+h1 {
+  color: $color-button;
+}
+
+p {
+  font-size: rem(60);
+  margin: 0;
+}
+
+a {
+  color: $color-menu-text;
+  font-size: $font-size-h2;
+  @include hover;
+}
+</style>
